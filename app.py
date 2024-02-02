0e8e90f63af2cdbaf07d0a12d679c3a33c238a2e
Jose Hernandez <josehdzibarra@hotmail.com>
2024-02-02 15:39:29 -0600

    create course is started

--- app.py
+++ app.py
@@ -9,3 +9,11 @@ def index():
 @app.route('/login',methods=['GET','POST'])
 def login():
     return render_template('login.html')
+
+@app.route('/signin',methods=['GET','POST'])
+def signin():
+    return render_template('signin.html')
+
+@app.route('/createcourse',methods=['GET','POST'])
+def createCourse():
+    return render_template('createcourse.html')
